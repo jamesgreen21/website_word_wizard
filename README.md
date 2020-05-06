ee25d7a9090f9624afc7891017d18e90fbd00b3c
James Green <jamesgreen210486@gmail.com>
2020-05-06 10:38:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#  Website Word Wizard
+
+Enter a URL to find out all words that display on the page along with their count
+
+## Installation
+
+Clone and install:
+
+```bash
+git clone https://github.com/jamesgreen21/website_word_wizard
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+import numpy as np
+import pandas as pd
 import requests
-import string
-import csv
+import re
+import operator
+import os
 
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template, send_from_directory, abort
 
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,68 +21,60 @@ def index():
     """
     context = {}
     if request.method == 'POST':
+        # Try URL or return message error
         try:
             url = request.form["url"]
             source = requests.get(url).text
-            results = word_counter(source)
-            context['results'] = results
-
-            # Create CSV
-            csv_file = open('word_count.csv', 'w',  newline='')
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(['word', 'count'])
-            for rec in results:
-                csv_writer.writerow([rec, results[rec]])
-            csv_file.close
-            context['filename'] = csv_file
-
         except:
             context['url_help'] = 'Please enter a valid URL'
+            return render_template('index.html', **context)
+
+        context['results'] = word_counter(source)
+        context['filename'] = 'word_count.csv'
 
     return render_template('index.html', **context)
 
 
-@app.route("/<path:filename>", methods=['GET', 'POST'])
+@app.route('/<filename>')
 def download_csv(filename):
-    """
-    Download a CSV file of the last word_counter() run
-    """
-    return Response(
-        filename,
-        mimetype="text/csv",
-        
-        )
+    try:
+        return send_from_directory(app.root_path, filename=filename, as_attachment=True)
+    except OSError:
+        abort(404)
 
 
-def tag_allowed(tag):
+def allowed_tag(tag):
     """
     Checks the tag for unwanted elements such as script tag data
     """
-    if tag.parent.name in ['html', 'style', 'script', 'head', 'title', 'meta', '[document]', '!DOCTYPE']:
+    if isinstance(tag, Comment) or tag.parent.name in [
+        'html',
+        'style',
+        'script',
+        'head',
+        'title',
+        'meta',
+        'document',
+        '!DOCTYPE'
+    ]:
         return False
-    if isinstance(tag, Comment):
-        return False
-    return True
+    else:
+        return True
 
 
-def word_counter(url):
+def word_counter(source):
     """
     Counts the words within a url parsed
     """
-    soup = BeautifulSoup(url, 'html.parser')
-    all_words = soup.findAll(text=True)
-
-    word_list = ', '.join(list(filter(tag_allowed, all_words))).split()
-    word_count_dict = {}
-    for word in word_list:
-        # Remove punctions from either end
-        if not word.isalnum():
-            continue          
-        first = 1 if word[0] in string.punctuation else 0
-        last = -1 if word[-1] in string.punctuation else None
-        word_count_dict[word[first:last].lower()] = word_count_dict.get(word[first:last].lower(), 0) + 1
-
-    # Order Dict
-    word_count_dict = {k: v for k, v in sorted(word_count_dict.items(), key=lambda item: item[1], reverse=True)}
-
-    return word_count_dict
+    soup = BeautifulSoup(source, 'html.parser')
+    text = soup.findAll(text=True)
+    # Remove unwanted tags and convert to str
+    text = ', '.join(list(filter(allowed_tag, text))).lower()
+    # Create key, val pairs
+    k, v = np.unique(np.array(re.sub(r'[^A-Za-z0-9 \']', '', text).split()), return_counts=True)
+    # Build df
+    df = pd.DataFrame(data={'count':v}, index=k).sort_values('count', ascending=False)
+    df.to_csv('word_count.csv')
+    # Build dict
+    words = dict(sorted(dict(zip(k, v)).items(), key=operator.itemgetter(1),reverse=True))
+    return words
